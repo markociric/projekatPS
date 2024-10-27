25df0fc80d0e77a69b680bb5163fd386707dbafb
Marko Ciric <marko.ciric2002@gmail.com>
2024-10-27 23:19:53 +0100

    Test dizajn Login i Register formi

---
+++ src/main/java/main/StartForm.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import forms.LoginForm;
+
+/**
+ *
+ * @author Marko
+ */
+public class StartForm {
+    
+    
+    public static void main(String[] args){
+       new LoginForm().setVisible(true);
+        
+    }
+}
